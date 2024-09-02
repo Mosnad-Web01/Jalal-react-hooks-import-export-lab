eeb35431c45825c642d07b520fa93ed9b25466db
qusailah <qusailahj@gmail.com>
2024-09-03 01:23:16 +0300

    feat: Added the solution

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,6 @@
 import React from "react";
-
-function About() {
+import { image } from "../data/user";
+ function About() {
   return (
     <div id="about">
       <h2>About Me</h2>
@@ -9,3 +9,4 @@ function About() {
     </div>
   );
 }
+export default About;
